804849452aab6499eb507764c481f58fd7a4dfb7
Lawrence-AIP <liuch@allinfinance.com>
2023-11-29 17:22:24 +0800

    更新题目

--- fromBook/C7.DFS/91.getGraphPaths.py
+++ fromBook/C7.DFS/91.getGraphPaths.py
@@ -2,4 +2,45 @@
 class GraphNode:
     def __init__(self, val):
         self.val = val
-        self.neighbor = []
+        self.nexts = []
+
+
+def count_paths(source, destination, steps):
+    # 如果步数为0，且源节点就是目的节点，则找到一条路径
+    if steps == 0:
+        return 1 if source == destination else 0
+    # 如果步数不为0，但是源节点没有相邻的节点，则无法继续走下去
+    if not source.nexts:
+        return 0
+    # 初始化路径数量为0
+    path_count = 0
+    # 遍历所有相邻的节点
+    for neighbor in source.nexts:
+        # 对每个相邻节点，递归地寻找剩余步数的路径
+        path_count += count_paths(neighbor, destination, steps - 1)
+    return path_count
+
+
+# 有向图的节点java定义如下：
+#      class GraphNode{
+#         int val;
+#         ArrayList<GraphNode> nexts;
+#      }
+
+# 使用示例
+# 假设我们有以下有向图:
+# A -> B, A -> C, B -> D, C -> D, D -> B
+# 我们要找从A到D，正好走3步的路径数量
+# 创建图节点
+A = GraphNode('A')
+B = GraphNode('B')
+C = GraphNode('C')
+D = GraphNode('D')
+
+# 建立连接
+A.nexts = [B, C]
+B.nexts = [D]
+C.nexts = [D]
+D.nexts = [B]
+
+print(count_paths(A, D, 2))
--- fromBook/C8.Hash/92.findLongestConsequence.py
+++ fromBook/C8.Hash/92.findLongestConsequence.py
@@ -1,1 +1,24 @@
 # 给出一个无序的整型数组，找出最长连续元素序列的长度。时间复杂度要求在线性时间内。
+def longest_consecutive(nums):
+    num_set = set(nums)  # 将数组转换为集合来去除重复元素，并且提供O(1)的查找时间
+    longest_streak = 0
+
+    for num in num_set:
+        # 只有当当前数字不是某个序列的中间部分时，才开始计算序列长度
+        if num - 1 not in num_set:
+            current_num = num
+            current_streak = 1
+
+            # 查找当前数字后面的连续数字
+            while current_num + 1 in num_set:
+                current_num += 1
+                current_streak += 1
+
+            # 更新最长序列的长度
+            longest_streak = max(longest_streak, current_streak)
+
+    return longest_streak
+
+
+nums = [100, 4, 200, 1, 3, 2]
+print(longest_consecutive(nums))  # 输出应该是4，因为最长连续序列是[1, 2, 3, 4]
